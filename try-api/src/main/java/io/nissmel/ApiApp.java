34296126263b5435e6f4a6c7ca9abe682da8bfaf
Łukasz <ukasz@192.168.0.67>
2019-09-02 17:45:53 +0200

    Begining!

---
+++ try-api/src/main/java/io/nissmel/ApiApp.java
@@ -0,0 +1,15 @@
+package io.nissmel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiApp.class, args);
+		
+	}
+
+}
