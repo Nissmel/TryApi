74235c0d187beb10c0689bd83ca14e741b5dd2ba
Łukasz <ukasz@192.168.0.67>
2019-09-03 17:13:01 +0200

    Topic service

--- try-api/src/main/java/io/nissmel/topic/TopicController.java
+++ try-api/src/main/java/io/nissmel/topic/TopicController.java
@@ -1,23 +1,47 @@
 package io.nissmel.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List <Topic> getAlleTopics()
 	{
-		return Arrays.asList
-				(
-					new Topic("Spring", "Spring Framework", "Spring Frameworks Description"),
-					new Topic("Java", "Java IDE", "Java IDE Description"),
-					new Topic("Python", "Python IDE", "Python IDE Description"),
-					new Topic("C++", "C++ IDE", "C++ IDE Description")	
-				);
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id)
+	{
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic)
+	{
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id)
+	{
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public void deleteTopic(@PathVariable String id)
+	{
+		topicService.deleteTopic(id);
 	}
 }
