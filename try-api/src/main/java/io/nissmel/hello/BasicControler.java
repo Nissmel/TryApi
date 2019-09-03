9b5e6f1af189f35f6e2cbe20132b5393f4e356e6
Łukasz <ukasz@192.168.0.67>
2019-09-03 17:36:42 +0200

    Final

--- try-api/src/main/java/io/nissmel/hello/BasicControler.java
+++ try-api/src/main/java/io/nissmel/hello/BasicControler.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloControler {
+public class BasicControler {
 
 	@RequestMapping("/hello")
 	public String sayHi()
 	{
-		return "Hi!";
+		return "Hi and welcome!";
 	}
 }
---
+++ try-api/src/main/java/io/nissmel/webSpring.java
@@ -0,0 +1,15 @@
+package io.nissmel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class webSpring {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(webSpring.class, args);
+		
+	}
+
+}
